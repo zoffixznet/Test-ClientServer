2a88fabaa634db1357349ca1bd2bd0baedf808a7
Anthony Parsons <flussence+git@gmail.com>
2012-12-05 01:42:28 +0000

    I have no idea what I'm doing right now.

--- lib/Test/ClientServer.pm
+++ lib/Test/ClientServer.pm
@@ -1,19 +1,42 @@
 class Test::ClientServer;
+# vim: set tw=80 :
+# This entire class is a workaround for not having &fork. The workaround is
+# completely insane and liable to break if you look at it funny. Don't try
+# anything more complicated than the module's own tests because horrible things
+# will happen.
 
-has &!client = fail 'client code not defined';
-has &!server = fail 'server code not defined';
+enum ForkDirection <parent server client>;
+
+has &!client = { fail 'client code not defined' };
+has &!server = { fail 'server code not defined' };
 has Int $!timeout = 60;
 
 method run() {
-    spork ?? &!client()
-          !! &!server()
-}
-
-sub spork() {
-    if &fork {
-        ??? ('This code is not Y2038-compliant. Please consult the manual.');
+    if !defined(my $pid = self.spork()) {
+        exit;
+    }
+    elsif $pid > 0 {
+        &!server({ sleep 1 });
     }
     else {
-        say $?FILE;
+        &!client({ sleep 5 });
     }
+}
+
+method spork() returns ForkDirection {
+    given %*ENV<TEST_CLIENTSERVER_FORK> // Nil {
+        return ForkDirection::server when 'server';
+        return ForkDirection::client when 'client';
+    }
+
+    run(q:c:to<FI>);
+        ( TEST_CLIENTSERVER_FORK="server" perl6 {$?FILE} ) & \
+        server_pid=$! \
+        ( TEST_CLIENTSERVER_FORK="client" perl6 {$?FILE} ) & \
+        client_pid=$! \
+        sleep {$!timeout} \
+        kill $server_pid $client_pid
+    FI
+
+    return ForkDirection::parent;
 }
