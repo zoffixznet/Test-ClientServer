53b31c4c5937d4ca21390976da583e7c3f955412
Anthony Parsons <flussence@gmail.com>
2014-08-29 00:32:38 +0100

    Refactor to get rid of convoluted code
    Timeouts should now throw a custom exception. Operative word is "should",
    because there's still a bug where they don't work, which I can't figure out...

--- lib/Test/ClientServer.pm
+++ lib/Test/ClientServer.pm
@@ -1,20 +1,28 @@
-class Test::ClientServer:auth<github:flussence>:ver<2.0.0-pre>;
+class Test::ClientServer:auth<github:flussence>:ver<2.0.0-pre.2>;
+
+class X::Test::ClientServer is Exception {
+    has Int $.timeout;
+    method message { "Test timed out after {$.timeout}s"; }
+}
 
 has &.server = ...;
 has &.client = ...;
 
-#| Note that this does NOT kill the test threads (it probably should); it falls out of the block.
+#| N.B. This doesn't (and can't) kill the test threads, because that's unsafe.
 has Int $.timeout = 30;
 
 method run() {
-    my $wait = Semaphore.new(0);
+    my Semaphore $wait .= new(0);
+    my $server = start { &.server.({ $wait.release }) };
+    my $client = start { &.client.({ $wait.acquire }) };
+    my $expire = Promise.in($.timeout);
 
-    # Server and client should invoke the given callbacks whenever they're ready to interact; this
-    # ensures the server is up before the client.
-    my @workers =
-        start({ &.server.({ $wait.release }) }),
-        start({ &.client.({ $wait.acquire }) });
+    for ^3 {
+        await Promise.anyof($server, $client, $expire);
 
-    # Wait $.timeout seconds, or until those have both exited
-    await Promise.anyof( Promise.in($.timeout), Promise.allof(|@workers) );
+        return if $server and $client;
+        die X::Test::ClientServer.new(:$.timeout) if $expire;
+    }
+
+    !!!
 }
--- t/03-timeout.t
+++ t/03-timeout.t
@@ -2,18 +2,18 @@
 use Test::ClientServer;
 use Test;
 
-plan 1;
-
-# Ensure that the timeout kills the processes in a reasonable amount of time
-my $start = now;
+plan 2;
 
 # Simulate a server taking too long to start
-.run given Test::ClientServer.new(
-    :timeout(3),
-    server => sub (&callback) { sleep 10; &callback(); diag 'Server code ran to end' },
-    client => sub (&callback) { &callback(); diag 'Client code ran to end' },
-);
+throws_like {
+    .run given Test::ClientServer.new(
+        :timeout(3),
+        server => sub (&callback) { sleep(10); &callback(); },
+        client => sub (&callback) { &callback(); },
+    );
+}, X::Test::ClientServer;
 
-my $end = now - $start;
+my $end = now - BEGIN now;
 
-ok(3 < $end < 10, 'Timeout works sanely') or diag("Failure: 3 < $end < 10");
+ok(3 < $end < 10, 'Timeout works sanely')
+    or diag("Failure: 3 < $end < 10");
